a9bb83d046529a935306fdcd5576caa0caa58cb4
Ajitesh Das <ajiteshdas@Ajiteshs-MacBook-Air.local>
2019-08-12 10:55:51 +0530

    add NLP models

--- NLP_models.py
+++ NLP_models.py
@@ -1,69 +1,180 @@
-# Classification template
-
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
 
-# Splitting the dataset into the Training set and Test set
+# cleaning texts
+import nltk
+import re
+nltk.download('stopwords')
+
+corpus = []
+
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+
+# creating the corpus by pre-processing the data
+for i in range(0, len(dataset['Review'])):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(
+        stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+from sklearn.feature_extraction.text import CountVectorizer
+# create your sparse matrix with all words from reviews
+cv = CountVectorizer(max_features=1500)
+
+# create your independent variable matrix
+X = cv.fit_transform(corpus).toarray()
+# create your dependent variable matrix
+y = dataset.iloc[:, 1].values
+
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+#compare different classification models
+metrics = [
+        'Accuracy',
+        'Precision',
+        'Recall',
+        'F1 Score'
+]
+classifierArray = [
+    'GaussianNB',
+    'Bernoulli_NB',
+    'Mutilnomial_NB',
+    'DECISION_TREE',
+    'RANDOM_FOREST',
+    'Sigmoid_SVM',
+    'Gaussian_SVM'
+]
 
-# Fitting classifier to the Training set
-from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
-classifier.fit(X_train, y_train)
+comparison_matrix = {
+        'GaussianNB':{
+            'Accuracy':[],
+            'Precision':[],
+            'Recall':[],
+            'F1 Score':[]
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
+        },
+        'Bernoulli_NB':{
+            'Accuracy':[],
+            'Precision':[],
+            'Recall':[],
+            'F1 Score':[]
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+        },
+        'Mutilnomial_NB':{
+            'Accuracy':[],
+            'Precision':[],
+            'Recall':[],
+            'F1 Score':[]
 
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Decision Tree (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+        },
+        'DECISION_TREE':{
+            'Accuracy':[],
+            'Precision':[],
+            'Recall':[],
+            'F1 Score':[]
 
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Decision Tree (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+        },
+        'RANDOM_FOREST':{
+            'Accuracy':[],
+            'Precision':[],
+            'Recall':[],
+            'F1 Score':[]
+
+        },
+        'Sigmoid_SVM':{
+            'Accuracy':[],
+            'Precision':[],
+            'Recall':[],
+            'F1 Score':[]
+
+        },
+        'Gaussian_SVM':{
+            'Accuracy':[],
+            'Precision':[],
+            'Recall':[],
+            'F1 Score':[]
+
+        }
+}
+
+
+def classifierFunc():
+    classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(y_test, y_pred)
+
+    tp, tn, fp, fn = cm[1,1], cm[0,0], cm[0,1], cm[1,0]
+
+    Accuracy = round((tp+tn)/(tp+tn+fp+fn)*100, 1)
+    Precision = round(tp / (tp + fp)*100,1)
+    Recall = round(tp / (tp + fn)*100,1)
+    F1_Score = round(2 * Precision * Recall / (Precision + Recall),2)
+
+    comparison_matrix[index][metrics[0]].append(Accuracy)
+    comparison_matrix[index][metrics[1]].append(Precision)
+    comparison_matrix[index][metrics[2]].append(Recall)
+    comparison_matrix[index][metrics[3]].append(F1_Score)
+
+def scaler():
+    # Feature Scaling
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    global X_train 
+    X_train = sc.fit_transform(X_train)
+    global X_test 
+    X_test = sc.transform(X_test)
+
+for index in classifierArray:
+    if (index == 'GaussianNB'):
+        from sklearn.naive_bayes import GaussianNB
+        classifier = GaussianNB()
+        classifierFunc()
+
+    elif (index == 'Bernoulli_NB'):
+        from sklearn.naive_bayes import BernoulliNB
+        classifier = BernoulliNB()
+        classifierFunc()
+
+    elif (index == 'Mutilnomial_NB'):
+        from sklearn.naive_bayes import MultinomialNB
+        classifier = MultinomialNB()
+        classifierFunc()
+
+    elif (index == 'DECISION_TREE'):
+        from sklearn.tree import DecisionTreeClassifier
+        classifier = DecisionTreeClassifier(criterion='entropy',random_state=0)
+        classifierFunc()
+        
+    elif (index == 'RANDOM_FOREST'):
+        from sklearn.ensemble import RandomForestClassifier
+        classifier = RandomForestClassifier(n_estimators=300, criterion='entropy',random_state=0)
+        classifierFunc()
+
+    elif (index == 'Sigmoid_SVM'):
+        from sklearn.svm import SVC
+        classifier = SVC(kernel='sigmoid', random_state=0)
+        scaler()
+        classifierFunc()
+
+    elif (index == 'Gaussian_SVM'):
+        from sklearn.svm import SVC
+        classifier = SVC(kernel='rbf', random_state=0)
+        scaler()
+        classifierFunc()
+
+#Create dataframe
+dataframe = pd.DataFrame(comparison_matrix)
+#dataframe.to_csv('Model_performance.csv')
